ebd8f975147e4dba4b229e242a5a92f8bc1607ce
Raul Demner <rauldemner.lopez@globant.com>
2022-05-10 10:00:38 -0500

    example of range

--- src/TypesDemo.kt
+++ src/TypesDemo.kt
@@ -1,8 +1,17 @@
 fun main(args: Array<String>) {
-    arrays();
+    arrays()
+    ranges()
 }
 
 fun arrays() {
     var names = arrayOf("Jhon", "Jane")
     println(names.toList())
+}
+
+fun ranges() {
+    val progression = 10 downTo 1
+    for (x in progression) {
+        println(x)
+    }
+
 }
