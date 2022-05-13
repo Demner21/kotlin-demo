b2b9e6379be3d252f65afc13752723702e9db462
Raul Demner <rauldemner.lopez@globant.com>
2022-05-13 11:48:34 -0500

    example of string template and when

--- src/TypesDemo.kt
+++ src/TypesDemo.kt
@@ -1,6 +1,15 @@
 fun main(args: Array<String>) {
-    arrays()
-    ranges()
+//    arrays()
+//    ranges()
+    using_when("3")
+    using_when("v")
+    using_when("z")
+
+    string_template("test string template")
+}
+
+fun string_template(message: String) {
+    println("the message is $message")
 }
 
 fun arrays() {
@@ -13,5 +22,14 @@ fun ranges() {
     for (x in progression) {
         println(x)
     }
+}
 
+fun using_when(value_to_evaluate: Any){
+    when (value_to_evaluate) {
+        "v" -> println("string case")
+        "3" -> println("case with number 3")
+        else -> {
+            println("default case ")
+        }
+    }
 }
