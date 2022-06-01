f37202d2b2ef735f7f54861de156623c30662a4f
Raul Demner <rauldemner.lopez@globant.com>
2022-05-31 19:59:51 -0500

    basic uses of sequences and predicates

--- src/demo/kotlin/collection/CollectionDemo.kt
+++ src/demo/kotlin/collection/CollectionDemo.kt
@@ -2,6 +2,18 @@ package demo.kotlin.collection
 
 fun main(args:Array<String>) {
     val sequence = generateSequence(2) { it + 2 }
-    val take = sequence.take(10)
-    println(take.toList())
+    val intSequence: Sequence<Int> = sequence.take(10)
+    println(intSequence.toList())
+    println("intSequence are > 2 ${intSequence.all { it > 2 }}" )
+    println("intSequence are odd ${intSequence.all { it % 2 == 0 }}" )
+
+    //using an empty sequence
+    println(emptySequence<Int>().any())
+    println("intSequence contains 20? ${intSequence.contains(20)}")
+    println("intSequence contains 30? ${intSequence.contains(30)}")
+    println("total numbers of elements of intSequence: ${intSequence.count()}")
+    println("total numbers of elements of intSequence > 4: ${intSequence.count { it > 4 }}")
+
+
+
 }
